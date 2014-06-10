68dece8d97f0182499540a5349dcde77e2707ad9
liming <liming@sogou-inc.com>
2014-06-10 18:18:47 +0800

    add guide

---
+++ WebContent/js/guide.js
@@ -0,0 +1,102 @@
+(function($){
+	var guideEditor = null;
+	var editorOptions = {
+			filterMode : true,
+			allowFileManager : true,
+			urlType : 'relative',
+			formatUploadUrl : false,
+			height: 600,
+			uploadImage:false,  //不上传图片，直接用base64
+			resizeType : 0
+	};
+	
+	function initAllGuide(){
+		$.ajax({
+			url:'../guide/getAllGuide.do',
+			async:false,
+			dataType:'json',
+			success:function(data){
+				var $allguide = $('#all_guide');
+				$allguide.empty();
+				
+				var template = '<li><font>{0}</font>&nbsp;<a href="#" id={1}>{2}</a></li>';
+				for(var i = 0,length = data.length; i < length; i++){
+					$allguide.append(String.format(template,i+1 , data[i].guideId,data[i].guideName));
+				}
+			}
+		});
+	}
+	
+	function bindEvents(){
+		bindClick();
+	}
+	
+	function bindClick(){
+		$('#all_guide').on('click','a',guideAClick);
+		
+		//保存说明
+		$('#save_editor').click(function(){
+			var guideId = $('#guide_id').val();
+			if(guideId === "")
+				return;
+			else{
+				var guidHtml = getSafeParam(guideEditor.html());
+				
+				$.ajax({
+					url:'../guide/saveGuideHtml.do',
+					type:'post',
+					data:{guideId:guideId,guideHtml:guidHtml},
+					success:function(data){
+						if(data === "true")
+							showInfoWin('success','保存成功!');
+						else
+							showInfoWin('error','保存失败!');
+					}
+				});
+			}
+		});
+	}
+	
+	function guideAClick(e){
+		var guideId = $(this).attr('id');
+		$('#guide_id').val(guideId);
+		
+		$('#all_guide li').removeClass("active");
+		$(this).closest("li").addClass("active");
+		
+		if(guideId){
+			$.ajax({
+				url:'../guide/getGuideHtml.do',
+				async:false,
+				dataType:'text',
+				data:{guideId:guideId},
+				success:function(data){
+					//是否需要data解析
+					var guideHtmlNode = $('#guide');
+					if(guideHtmlNode.length > 0){
+						guideHtmlNode.empty();
+						guideHtmlNode.html(data);
+					}
+					
+					if(guideEditor)
+						guideEditor.html(data);
+				}
+			});
+		}
+	}
+	
+	$(function(){
+		var guideId = request('guideId');
+		bindEvents();
+		initAllGuide();
+		
+		if(KindEditor)
+			guideEditor = KindEditor.create('textarea[id="guide_editor"]', editorOptions);
+		
+		if(guideId){
+			$("#all_guide a[id=" + guideId + "]").click();
+		}
+	});
+	
+	
+})(jQuery);
